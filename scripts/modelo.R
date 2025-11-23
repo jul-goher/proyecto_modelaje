2670a581f9a628ba905c7f80f595c5613412b030
jul-goher <juliegonzalezhrra@gmail.com>
2025-11-23 17:07:19 -0600

    cambios modelo

--- scripts/modelo.R
+++ scripts/modelo.R
@@ -3,37 +3,38 @@ library(deSolve)
 
 SEIHRV <- function (t, estado, parametros) {
   with (as.list (c(estado, parametros)) , {
-    dS <- mu - beta*S - gamma*R + rho*V - mu*S
-    dE <- beta*S - sigma*rho*E - sigma*E*(1 - rho) - mu*E
-    dI <- sigma*(1-rho)*E - alpha*I - (mu + K)*I
-    dG <- sigma*rho*E - delta*G - (mu + K)*G
+    dS <- mu - beta*S - gamma*p*R + rho*V - mu*S
+    dE <- beta*S - sigma*p*E - sigma*E*(1 - p) - mu*E
+    dI <- sigma*(1-p)*E - alpha*I - (mu + K)*I
+    dG <- sigma*p*E - delta*G - (mu + K)*G
     dH <- delta*G - epsilon*H - mu*H
-    dR <- epsilon*H + alpha*I - gamma*R - mu*R - lambda*R
-    dV <- lambda*R - rho*V - mu*V
+    dR <- epsilon*H + alpha*I - gamma*p*R - mu*R - lambda*(1-p)*R
+    dV <- lambda*(1-p)*R - rho*V - mu*V
     list(c(dS, dE, dI, dG, dH, dR, dV))
   })
 }
 
 #### DENV 1 
-pars_d1 <- c ( mu =0.0000169 , beta = 0.048, gamma = 0.00063, rho = 0.4, 
+pars_d1 <- c ( mu =0.0000169 , beta = 0.048, gamma = 0.00063, rho = 0.543 , 
                sigma = 0.1 , alpha = 0.657 , delta = 0.05, 
-               epsilon = 0.657, lambda = 7.0002e-05, K = 0.001)
+               epsilon = 0.657, lambda = 7.0002e-05, K = 0.001, p = 0.4)
 condiciones_d1 <- c (S= 1000000, E = 10, I = 1000, 
                      G = 30, H = 10, R = 15 , V = 30)
-tiempo_d1 <- seq (0, 100, by = 0.05)
+tiempo_d1 <- seq (0, 60, by = 0.05)
 out_d1 <- ode(condiciones_d1, tiempo_d1, SEIHRV, pars_d1) 
 
 matplot(out_d1 [ , 1], out_d1 [ , 2:8], type = "l", xlab = "tiempo", 
         ylab = "Población", main = "DENV-1", lwd = 2)
 legend ("topright", c("Susceptibles", "Expuestos", "I_Leves",
-                      "I_Graves", "Hospitalizados", "Recuperados", "Vacunados"), 
+        "I_Graves", "Hospitalizados", "Recuperados", "Vacunados"), 
         col = 1:4, lty = 1:3, cex= 0.5)
 
-##### DENV 2 
 
-pars_d2 <- c (mu = 0.0000169, rho = 0.4, beta = 0.056, gamma = 0.00042, sigma = 0.1 , alpha = 0.552 , delta = 0.05, epsilon = 0.552, lambda = 7.0002e-05, K = 0.001)
+
+##### DENV 2 
+pars_d2 <- c (mu = 0.0000169, p = 0.4, beta = 0.056, gamma = 0.00042, sigma = 0.1 , alpha = 0.552 , delta = 0.05, epsilon = 0.552, lambda = 7.0002e-05, K = 0.001, rho = 0.543)
 condiciones_d2 <- c (S= 100000, E=50, I=10, G=2, H=3, R=3 , V = 2)
-tiempo_d2 <- seq (0, 100, by = 0.05)
+tiempo_d2 <- seq (0, 60, by = 0.05)
 out_d2 <- ode(condiciones_d2, tiempo_d2, SEIHRV, pars_d2) 
 
 matplot(out_d2 [ , 1], out_d2 [ , 2:8], type = "l", xlab = "tiempo", ylab = "Población", 
@@ -44,9 +45,9 @@ legend ("topright", c("Susceptibles", "Expuestos", "I_Leves", "I_Graves", "Hospi
 
 
 ##### DENV 3
-pars_d3 <- c ( mu =0.0000169 , beta = 0.11, gamma = 0.00056, rho = 0.4, sigma = 0.1 , alpha = 0.657 , delta = 0.05 , epsilon = 0.882, lambda = 7.0002e-05, K = 0.001)
+pars_d3 <- c ( mu =0.0000169 , beta = 0.11, gamma = 0.00056, p = 0.4, sigma = 0.1 , alpha = 0.657 , delta = 0.05 , epsilon = 0.882, lambda = 7.0002e-05, K = 0.001, rho = 0.543)
 condiciones_d3 <- c (S= 100000, E=50, I=10, G=2, H=3, R=3 , V = 2)
-tiempo_d3 <- seq (0, 100, by = 0.05)
+tiempo_d3 <- seq (0, 60, by = 0.05)
 out_d3 <- ode(condiciones_d3, tiempo_d3, SEIHRV, pars_d3) 
 
 matplot(out_d3 [ , 1], out_d3 [ , 2:8], type = "l", xlab = "tiempo", ylab = "Población", 
@@ -57,20 +58,13 @@ legend ("topright", c("Susceptibles", "Expuestos", "I_Leves", "I_Graves", "Hospi
 
 
 ##### DENV 4 
-pars_d4 <- c ( mu =0.0000169 , beta = 0.081, gamma = 0.00053, rho = 0.4, sigma = 0.1, alpha = 0.687 , delta = 0.05, epsilon = 0.687, lambda = 7.0002e-05, K = 0.001)
+pars_d4 <- c ( mu =0.0000169 , beta = 0.081, gamma = 0.00053, p = 0.4, sigma = 0.1, alpha = 0.687 , delta = 0.05, epsilon = 0.687, lambda = 7.0002e-05, K = 0.001, rho = 0.543)
 condiciones_d4 <- c (S= 100000, E=50, I=10, G=2, H=3, R=3 , V = 2)
-tiempo_d4 <- seq (0, 100, by = 0.05)
+tiempo_d4 <- seq (0, 60, by = 0.05)
 out_d4 <- ode(condiciones_d4, tiempo_d4, SEIHRV, pars_d4) 
 
 matplot(out_d4 [ , 1], out_d4 [ , 2:8], type = "l", xlab = "tiempo", ylab = "Población", 
         main = "DENV-4", lwd = 2)
 legend ("topright", c("Susceptibles", "Expuestos", "I_Leves", "I_Graves", "Hospitalizados", "Recuperados", "Vacunados"), 
         col = 1:4, lty = 1:3, cex= 0.5)
-
-
-
-
-
-
-
 
